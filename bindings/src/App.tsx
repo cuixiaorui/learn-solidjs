9235ce163581f30beed19c54a0782347d95917ee
cuixiaorui <cui_xiaorui@126.com>
2022-07-21 10:14:40 +0800

    feat: add bindings&events

---
+++ bindings/src/App.tsx
@@ -0,0 +1,23 @@
+import { Component, createSignal } from "solid-js";
+
+const App: Component = () => {
+  const [pos, setPos] = createSignal({ x: 0, y: 0 });
+
+  const mouseMoveHandler = (data, e) => {
+    console.log(data);
+    // 这里注意  第一个参数是传过来的 data
+    setPos({
+      x: e.clientX,
+      y: e.clientY,
+    });
+  };
+  return (
+    // 如果你想给函数传参数的话  那么得用数组的形式 然后把参数放到第二个位置
+    <div onMouseMove={[mouseMoveHandler, "heihei"]}>
+    {/* <div onMouseMove={mouseMoveHandler}> */}
+      The mouse position is {pos().x}:{pos().y}
+    </div>
+  );
+};
+
+export default App;
